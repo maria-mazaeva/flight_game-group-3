48626a8585cc26e880f4f9eb9903ba88c73457bf
NikaDeg <garelskaiavv1@gmail.com>
2025-10-01 10:51:30 +0300

    some changes

--- flight_game.py
+++ flight_game.py
@@ -6,7 +6,7 @@ connection = mysql.connector.connect(
     host="127.0.0.1",
     port=3306,
     database="flight_game",
-    user="nika",
+    user="root",
     password="password",
     autocommit=True
 )
